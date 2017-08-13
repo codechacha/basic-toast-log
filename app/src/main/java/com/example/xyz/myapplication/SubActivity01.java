59eb25bdefa0c5285ffe710fe1796ef6f16b9d16
Jeongsik Mun <farfs.dev@gmail.com>
2017-08-13 16:35:48 +0900

    start-activities

---
+++ app/src/main/java/com/example/xyz/myapplication/SubActivity01.java
@@ -0,0 +1,17 @@
+package com.example.xyz.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by XYZ on 2017-08-13.
+ */
+
+public class SubActivity01 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub01);
+    }
+}
