c0c0782da9c9662e43c90e5f9be700831fbdf49e
Jeongsik Mun <farfs.dev@gmail.com>
2017-08-15 19:07:53 +0900

    toast and log

--- app/src/main/java/com/example/xyz/myapplication/MainActivity.java
+++ app/src/main/java/com/example/xyz/myapplication/MainActivity.java
@@ -3,19 +3,24 @@ package com.example.xyz.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.v(TAG, "onCreate: activity가 실행되었습니다.");
     }
 
     void onButtonClick(View v) {
         Intent intent = new Intent(this, SubActivity01.class);
         startActivity(intent);
-    }
 
+        Log.d(TAG, "onButtonClick: SubActivity01을 실행시켰습니다.");
+    }
 }
--- app/src/main/java/com/example/xyz/myapplication/SubActivity01.java
+++ app/src/main/java/com/example/xyz/myapplication/SubActivity01.java
@@ -1,17 +1,31 @@
 package com.example.xyz.myapplication;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by XYZ on 2017-08-13.
  */
 
 public class SubActivity01 extends AppCompatActivity {
+    public static final String TAG = "SubActivity01";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub01);
+
+        Context context = getApplicationContext();
+        CharSequence text = "SubActivity01이 실행되었습니다";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+
+        Log.d(TAG, "onCreate: SubActivity01이 실행되었습니다.");
     }
 }
